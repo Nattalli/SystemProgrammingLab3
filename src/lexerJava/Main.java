20d78b02d18194e0f3eb7f78415c0b56f9ed6faf
Nataliia Zakharchuk <dominok56@gmail.com>
2021-11-15 22:00:29 +0200

    final version

---
+++ src/lexerJava/Main.java
@@ -0,0 +1,31 @@
+package lexerJava;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the filepath:");
+        String filename = sc.nextLine();
+
+        Path path = Paths.get(filename);
+        File file = path.toFile();
+
+        try
+        {
+            Lexer lexer = new Lexer(file);
+            System.out.println(lexer.toString());
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!");
+        }
+
+    }
+}
